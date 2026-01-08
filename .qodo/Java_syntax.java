a0aec66b30b71331a116b8a3fdf2e24fa6edd3e5
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-08 23:33:02 +0530

    first commit

---
+++ .qodo/Java_syntax.java
@@ -0,0 +1,5 @@
+class Java_syntax {
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+}
+}
---
+++ .qodo/java_commants.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    // This is a  single-line comment
+    System.out.println("Hello World");
+    /*
+     This is a multi-line comment
+     It can span multiple lines
+    */
+   System.out.println("Hello Again");
+  }
+}
