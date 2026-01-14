e48a85f6bf5463e7efa3bb0523821d8c469d7245
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-14 22:17:25 +0530

    Add files via upload

---
+++ do_while_example.java
@@ -0,0 +1,9 @@
+public class do_while_example {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
