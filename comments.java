94700c34045373e2ac06ad38fb25efbb2260b6a5
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-15 19:34:38 +0530

    Add files via upload

---
+++ comments.java
@@ -0,0 +1,17 @@
+public class comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         This is a multi-line comment.
+         It can span multiple lines.
+        */
+
+        /**
+         * This is a documentation comment.
+         * It is used to describe classes, methods, and fields.
+         */
+
+        System.out.println("Comments in Java example.");
+    }
+}
