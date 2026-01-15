f5ed206a3d2ee0565f0bf0adf271edf0d3a4cd10
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-15 20:02:52 +0530

    Add files via upload

---
+++ Nested_else_example.java
@@ -0,0 +1,15 @@
+public class Nested_else_example {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            if (number < 0) {
+                System.out.println("The number is negative.");
+            } else {
+                System.out.println("The number is zero.");
+            }
+        }
+    }
+}
