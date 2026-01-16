37cc483b87c0701f551bfe39ba5e86e3a6b441a7
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-16 22:05:23 +0530

    Add files via upload

---
+++ RandomNumberExample1.java
@@ -0,0 +1,7 @@
+public class RandomNumberExample1 {
+    public static void main(String[] args) {
+        // Generate a random number between 0 and 100
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println("Random Number: " + randomNum);
+    }
+}
