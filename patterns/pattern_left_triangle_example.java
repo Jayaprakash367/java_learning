04ab21917bd9eb0e1bcebb5349c126082af2f887
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-26 18:47:52 +0530

    Add files via upload

---
+++ patterns/pattern_left_triangle_example.java
@@ -0,0 +1,14 @@
+public class pattern_left_triangle_example {
+    public static void main(String[] args) {
+        int i, j, row = 6;
+        for (i = 0; i < row; i++) {
+            for(j=2*(row-i);j>=0;j--){
+                System.out.print(" ");
+            }
+            for (j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+    }
+}
+}
