500681d60e9b156e32ed1b01a62ada98516ff0e2
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-07 19:02:17 +0530

    Add files via upload

---
+++ Single_Inheritance.java
@@ -0,0 +1,20 @@
+package OOPs_Concept;
+
+public class Single_Inheritance {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class a{
+	 void Example1() {
+		
+	}
+}
+class b extends a{
+	 void num1() {
+		
+	}
+}
