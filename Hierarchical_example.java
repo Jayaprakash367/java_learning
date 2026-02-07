00fcbe2b63999a398dd3b97345c73a6bd96c8334
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-07 19:02:42 +0530

    Add files via upload

---
+++ Hierarchical_example.java
@@ -0,0 +1,25 @@
+package OOPs_Concept;
+
+public class Hierarchical_example {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class hello1{
+	void Example1() {
+		
+	}
+}
+class hello2 extends hello1{
+	void Example2() {
+		
+	}
+}
+class hello3 extends hello1{
+	void Example3() {
+		
+	}
+}
