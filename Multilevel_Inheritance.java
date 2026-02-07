ac70dfd24fdff5b8160c1eae7262f36fbe5eb30d
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-07 19:03:52 +0530

    Add files via upload

---
+++ Multilevel_Inheritance.java
@@ -0,0 +1,32 @@
+package OOPs_Concept;
+
+public class Multilevel_Inheritance {
+
+	public static void main(String[] args) {
+		new1 obj=new new1();
+		obj.Example1();
+
+	}
+
+}
+class new1{
+	void Example1() {
+		System.out.println("hello");
+	}
+}
+class new2 extends new1{
+	void Example2() {
+		
+	}
+}
+class new3 extends new2{
+	void Example3() {
+		
+	}
+}
+class new4 extends new3{
+	void Example4() {
+		
+	}
+}
+
