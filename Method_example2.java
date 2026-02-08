66347be2d04736f506641efd0c2f4b05150065ae
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-08 20:28:14 +0530

    Add files via upload

---
+++ Method_example2.java
@@ -0,0 +1,29 @@
+package OOPs_Concept;
+
+public class Method_example2 {
+
+	public static void main(String[] args) {
+		Method1();
+
+	}
+	public static void Method1() {
+		System.out.println("Lolii is : ");
+		Method2();
+	}
+public static void Method2() {
+		System.out.println("Dinesh is :");
+		Method3();
+	}
+public static void Method3() {
+	System.out.println("Prajith is :");
+	Method4();
+}
+public static void Method4() {
+	System.out.println("Sem is :");
+	Method5();
+}
+public static void Method5() {
+	System.out.println("Ajin is :");
+}
+
+}
