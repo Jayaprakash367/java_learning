ed69d7a461d1811c3e56c3e26c029cfc40c92ce1
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-15 22:17:38 +0530

    Add files via upload

---
+++ While_example.java
@@ -0,0 +1,14 @@
+package Core_java_program;
+
+public class While_example {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
