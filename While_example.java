c082e757ba049ed919b3149dd52ec3cb3f10bea0
jayaprakash367 <157467388+Jayaprakash367@users.noreply.github.com>
2026-08-10 22:07:57 +0530

    Update While_example.java

--- While_example.java
+++ While_example.java
@@ -1,14 +1,9 @@
-package Core_java_program;
-
-public class While_example {
-
-	public static void main(String[] args) {
-		int i=0;
-		while(i<10) {
-			System.out.println(i);
-			i++;
-		}
-
-	}
-
+public class while_example {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Count is: " + count);
+            count++;
+    }
+}
 }
